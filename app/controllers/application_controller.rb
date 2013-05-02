326b030ec26f15caf6ba4dff6e30304d043efa95
jonnii <me@jonnii.com>
2013-05-01 21:52:35 -0400

    oops, this isnt rails 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     if Rails.env.production?
-      http_basic_authenticate_with :name => "octopus", :password => "octopus"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "octopus" && password == "octopus"
+      end
     end
   end
 end
