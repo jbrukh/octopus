866d56eb56a487fed97be0da26bac556d565bb5b
jonnii <me@jonnii.com>
2013-05-01 23:09:42 -0400

    set auth token when creating user, add devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :token_authenticatable, :confirmable, :lockable
+
+  before_create :reset_authentication_token
 end
