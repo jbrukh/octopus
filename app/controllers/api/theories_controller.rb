64d1022e93505ff1c272c79093576d450f1596e2
jonnii <me@jonnii.com>
2013-05-07 12:51:43 +0100

    re-enable theories authentication

--- app/controllers/api/theories_controller.rb
+++ app/controllers/api/theories_controller.rb
@@ -1,5 +1,5 @@
 class Api::TheoriesController < ApplicationController
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   def index
     @theories = Theory.all
--- spec/controllers/api/theories_controller_spec.rb
+++ spec/controllers/api/theories_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Api::TheoriesController do
+  describe 'as a guest' do
+    describe '#index' do
+      before do
+        get :index
+      end
 
+      it { redirect_to new_user_session_url }
+    end
+  end
 end
