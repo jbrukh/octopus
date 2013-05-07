d5b82804f4dd70a28515c2b4147b645879ce3705
jonnii <me@jonnii.com>
2013-05-07 14:27:59 +0100

    more testing stuff, remembering this stuff without google is hard

--- spec/controllers/api/theories_controller_spec.rb
+++ spec/controllers/api/theories_controller_spec.rb
@@ -3,11 +3,24 @@ require 'spec_helper'
 describe Api::TheoriesController do
   describe 'as a guest' do
     describe '#index' do
-      before do
+      before :each do
+        get :index
+      end
+      it { should redirect_to new_user_session_url }
+    end
+  end
+
+  describe 'as a user' do
+    before :each do
+      sign_in users(:bob)
+    end
+
+    describe '#index' do
+      before :each do
         get :index
       end
 
-      it { redirect_to new_user_session_url }
+      it { should_respond_with :success }
     end
   end
 end
