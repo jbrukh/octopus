234c60a49c3217c44c12d88d5c8b1fe714edd093
jonnii <me@jonnii.com>
2013-05-15 00:02:05 -0400

    sse stuff

--- app/assets/javascripts/lib/message_bus.coffee
+++ app/assets/javascripts/lib/message_bus.coffee
@@ -1,9 +1,6 @@
 App.MessageBus = Em.Object.extend
   start: ->
-    url = "ws://#{window.location.host}/api/events"
-
-    console.log "Connecting to message bus: #{url}"
-
-    #socket = new WebSocket(url)
-    #socket.onmessage = (event) ->
-    #  console.log JSON.parse(event.data)
+    console.log "Connecting to message bus"
+    #source = new EventSource('/api/events')
+    #source.addEventListener 'recording.updated', (e) ->
+    #  console.log e
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,19 +1,25 @@
 class Api::EventsController < ApplicationController
-  #include Tubesock::Hijack
+  include ActionController::Live
 
-  #def index
-  #  hijack do |tubesock|
-  #    tubesock.onopen do
-  #      tubesock.send_data message: "streaming"
+  def index
+    response.headers['Content-Type'] = 'text/event-stream'
 
-  #      redis = Redis.new(:host => 'localhost', :port => 6379)
-  #      redis.subscribe('recording.updated') do |on|
-  #        on.message do |event, data|
-  #          logger.debug data
-  #          tubesock.send_data message: data
-  #        end
-  #      end
-  #    end
-  #  end
-  #end
+    redis = Redis.new :host => 'localhost', :port => 6379
+    redis.subscribe('recording.updated') do |on|
+      on.message do |event, data|
+        logger.debug data
+        response.stream.write(sse({data: data}, {event: 'recording.updated'}))
+      end
+    end
+
+    rescue IOError
+      # client disconnect
+    ensure
+    response.stream.close
+  end
+
+  private
+    def sse(object, options = {})
+      (options.map{|k,v| "#{k}: #{v}" } << "data: #{JSON.dump object}").join("\n") + "\n\n"
+    end
 end
