feb44addba2114b85ff0490add57782b1f28f3b1
jonnii <me@jonnii.com>
2013-05-17 20:15:56 -0400

    remove http authentication'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_filter :authenticate
-
-  def authenticate
-    if Rails.env.production?
-      authenticate_or_request_with_http_basic do |username, password|
-        username == "octopus" && password == "octopus"
-      end
-    end
-  end
 end
