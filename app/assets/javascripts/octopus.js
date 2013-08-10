413403b3eb8cc1afdfab87d01a804cc7657e82d2
jonnii <me@jonnii.com>
2013-08-09 21:08:20 -0400

    whitespace

--- app/assets/javascripts/octopus.js
+++ app/assets/javascripts/octopus.js
@@ -25,9 +25,11 @@
 //= require bootstrapped
 //= require_self
 //= require brain
+
 App = Ember.Application.create({
   LOG_TRANSITIONS: true,
   LOG_VIEW_LOOKUPS: true,
   LOG_ACTIVE_GENERATION: true
 });
+
 //= require_tree .
