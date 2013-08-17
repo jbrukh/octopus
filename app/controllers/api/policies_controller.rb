dcfbb80e52d9703472f934b6e0ac517f50540250
jonnii <me@jonnii.com>
2013-08-17 17:25:11 -0400

    update policy

--- app/controllers/api/policies_controller.rb
+++ app/controllers/api/policies_controller.rb
@@ -1,13 +1,6 @@
-require 'aws-sdk'
-
 class Api::PoliciesController < ApplicationController
-  def index
-    policy = AWS::Core::Policy.new
-    policy.allow(
-      :actions => ['s3:PutObject'],
-      :resources => "arn:aws:s3:::mybucket/mykey/*",
-      :principals => :any
-    ).where(:acl).is("public-read")
+  def show
+   policy = s3_upload_policy_document
 
     encoded = Base64.encode64(policy.to_json).gsub("\n","")
     signature = Base64.encode64(
@@ -16,6 +9,21 @@ class Api::PoliciesController < ApplicationController
           ENV['S3_SECRET_ACCESS_KEY'] || 'abcd', encoded)
       ).gsub("\n","")
 
-    render json: {:encoded => policy.to_json, :signature => signature}
+    render json: {:policy => policy.to_json, :signature => signature}
+  end
+
+private
+
+  def s3_upload_policy_document
+    {
+      expiration: 30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+      conditions: [
+        { bucket: 'erl-octopus-staging' },
+        { acl: 'private' },
+        ["starts-with", "$key", "recordings/"],
+        { success_action_status: 'http://localhost/' },
+        ["content-length-range", 0, 104857600]
+      ]
+    }
   end
 end
