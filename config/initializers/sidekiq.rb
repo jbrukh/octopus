515a40f7648584f027c683fe26286a92a2c0119a
Jonathan Goldman <me@jonnii.com>
2013-08-31 18:41:02 -0400

    OH LOOK A NAMESPACE THANKS YOU FUCK

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 unless Rails.env.development?
   Sidekiq.configure_server do |config|
-    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'octopus' }
+    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => '' }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'octopus' }
+    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => '' }
   end
 end
