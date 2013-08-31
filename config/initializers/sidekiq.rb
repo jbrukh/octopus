2efc44d439dddc61bb403973b294c87470b0c2b1
Jonathan Goldman <me@jonnii.com>
2013-08-31 18:47:33 -0400

    totally kill the namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 unless Rails.env.development?
   Sidekiq.configure_server do |config|
-    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => '' }
+    config.redis = { :url => ENV['REDIS_PROVIDER'] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => '' }
+    config.redis = { :url => ENV['REDIS_PROVIDER'] }
   end
 end
